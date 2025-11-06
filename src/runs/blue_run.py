ab9ed379ac391a783fd108b59c84efc0a72f6a88
tomas <bocan.tomas.6666@gmail.com>
2025-11-06 17:57:40 +0100

    commit 6.11.

--- src/runs/blue_run.py
+++ src/runs/blue_run.py
@@ -4,3 +4,10 @@ from movement import Movement
 
 def blue_run(robot: Robot, mv: Movement):
     debug_log("Starting Blue Run")
+
+    mv.straight(-20)
+    mv.reset_gyro()
+    robot.left_motor.reset_angle(0)
+    robot.right_motor.reset_angle(0)
+
+    
--- src/runs/orange_run.py
+++ src/runs/orange_run.py
@@ -3,35 +3,70 @@ from robot import Robot
 from movement import Movement
 
 rightSpeed: int = 200
-leftSpeed: int = 300
+leftSpeed: int = 600
 
 def orange_run(robot: Robot, mv: Movement):
     debug_log("Starting Orange Run")
+
+    mv.straight(-20)
     mv.reset_gyro()
     robot.left_motor.reset_angle(0)
     robot.right_motor.reset_angle(0)
 
     robot.right_motor.run_target(rightSpeed, 60)
-    mv.straight(340)
+    mv.straight(320)
 
     for x in range(3):
         robot.right_motor.run_target(10000, 0)
         robot.right_motor.run_target(10000, 60)
     robot.right_motor.run_target(10000, 0)
-    robot.right_motor.run_target(10000, 38)
+    robot.right_motor.run_target(10000, 30)
 
     mv.turn(-35)
-    robot.left_motor.run_target(leftSpeed, -120)
-    mv.straight(290)
+    robot.left_motor.run_target(leftSpeed, -90)
+    mv.straight(310)
 
     mv.turn(80)
-    mv.straight(60)
+    mv.straight(50)
 
-    mv.turn(-20)
-    mv.turn(20)
+    mv.turn(-25)
+    mv.turn(25)
     robot.right_motor.run_target(rightSpeed, 60)
     robot.left_motor.run_target(leftSpeed, -60)
 
-    mv.straight(100)
+    mv.straight(150)
+    mv.turn(10)
+    robot.left_motor.run_target(leftSpeed, -200)
+    mv.straight(-150)
+
+    mv.turn(-mv.pose.angle)
+
+    mv.straight(5)
+    
+    robot.right_motor.run_target(rightSpeed, -100)
+    robot.right_drive.run_angle(1000, 200)
+
+    mv.turn(-25)
+
+    mv.straight(-100)
+
+    mv.turn(-50)
+    mv.straight(200)
+
+    robot.left_motor.run_target(leftSpeed, -60)
+    robot.left_motor.run_target(leftSpeed, -200)
+
+    mv.turn(-60)
+    mv.turn(60)
+    mv.straight(-100)
+
+    mv.turn(-90)
+    mv.straight(200)
+
+
+
+
+
+
     
     
