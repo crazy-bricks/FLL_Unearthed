456e27f295692ec2fbd37a2a7e929f8e7860cefc
crazybricksteam <crazybricks.fllteam@gmail.com>
2026-03-08 22:50:50 +0100

    RED 100% funkcional

--- src/runs/red_run.py
+++ src/runs/red_run.py
@@ -80,19 +80,21 @@ def red_run(robot: Robot, mv: Movement):
     if run == 1:
         """Remove thing from the basket"""
         #ku
-        mv.straight(260)
+        mv.turn(-110) 
+        mv.straight(270)
         mv.turn(-100, speed=600)
         mv.straight(160)
         #nabírání basket
-        mv.turn(-150)
-        mv.straight(-75)
+        mv.turn(-159)
+        mv.straight(-80)
         wait(500)
         #ku crane
-        mv.straight(130)
-        mv.turn(-170)
-        mv.straight(120)
-        mv.turn(-190)
-        mv.straight(55)
+        mv.straight(80)
+        mv.turn(-160)
+        mv.straight(80)
+        mv.turn(-185)
+        mv.straight(60)
+        mv.turn(-200)
         #do mision
         mv.robot.left_motor.run(-600)
         wait(2000)
@@ -102,49 +104,48 @@ def red_run(robot: Robot, mv: Movement):
         mv.turn(-55)
         #statue
         mv.move_right_to(600, 200)
-        mv.straight(100)
-        mv.turn(-40)
+        mv.straight(125)
+        mv.turn(-45)
+        mv.move_right_to(600, 100)
+
+        mv.turn(-110)
+        mv.straight(700, cruise_speed=1600, accel_ratio=2.0)
         
 
         wait(100000)
-       
 
     else:
-        mv.turn(-90)
-        mv.straight(20)
-        mv.turn(-180)
-        mv.straight(95)
-        mv.turn(-190)
-        """Do rope mission"""
-        mv.move_left_to(10000, -1300)
-        mv.straight(-90)
-        """get redy for statue"""
-        mv.turn(-55)
+        """X mision"""
+        #ku
+        mv.turn(-110) 
+        mv.straight(140)
+        mv.turn(-100, speed=600)
+        mv.straight(260, cruise_speed=1600)
+        mv.turn(-150, speed= 300)
+        mv.straight(100)
+        mv.turn(-185)
+        mv.straight(70)
+        mv.turn(-200)
+        wait(100)
+
+        mv.robot.left_motor.run(-600)
+        wait(500)
+        mv.robot.left_motor.stop()
+
+        #wait(100000)
+
+        #do mision
+        mv.robot.left_motor.run(-600)
+        wait(2000)
+        mv.robot.left_motor.stop()
+        #back up
+        mv.straight(-100)
+        mv.turn(-57)
+        #statue
         mv.move_right_to(600, 200)
-        mv.straight(20)
-        #wait(10000)
+        mv.straight(120)
+        mv.turn(-40)#todle se menilo zbyenk
+        mv.move_right_to(600, 100)
 
-    #wait(100000)
-
-    """Return home"""
-    
-
-    wait(100)
-
-    mv.straight(160, 100)
-    mv.turn(-40)
-    mv.move_right_to(600, 100)
-
-    if run == 2:
-        mv.move_right_to(600, 160)
-
-    wait(100)
-
-    mv.straight(-120)
-    mv.turn(-75)
-    mv.move_right_to(800, 0)
-
-    mv.steady(-100, speed=1100)
-
-    mv.turn(-104)
-    mv.steady(-1200, speed=1100)
+        mv.turn(-110)
+        mv.straight(700, cruise_speed=1600, accel_ratio=2.0)
